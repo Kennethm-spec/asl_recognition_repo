4416f7eac36d446ec7e880778354bf06727100d0
KennethMunyuza-hope <kenneth.munyuza@hope.edu>
2023-05-09 14:13:38 -0400

    initial js integration

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,68 @@ const replayButton = document.getElementById('replayBtn');
 const approveButton = document.getElementById('approve');
 const timerElement = document.getElementById('timer');
 const resultElement = document.getElementById('result');
+const resultDiv = document.getElementById("result");
+
+let stream;
+let interval;
+let recognizedSigns = [];
+
+// startButton.addEventListener("click", async () => {
+//     startButton.disabled = true;
+//     stopButton.disabled = false;
+
+//     stream = await navigator.mediaDevices.getUserMedia({ video: true });
+//     video.srcObject = stream;
+
+//     interval = setInterval(captureAndSendFrame, 1000); // Send a frame every 1000ms (1 second)
+//     // TODO for Kenneth, maybe send images every 100 ms then server will just wait until 1 second
+//     // this will give server 10 images to work with.
+// });
+
+// stopButton.addEventListener("click", () => {
+//     startButton.disabled = false;
+//     stopButton.disabled = true;
+
+//     clearInterval(interval);
+//     video.srcObject.getTracks().forEach(track => track.stop());
+// });
+
+// async function captureAndSendFrame() {
+//     const canvas = document.createElement("canvas");
+//     canvas.width = video.videoWidth;
+//     canvas.height = video.videoHeight;
+//     const context = canvas.getContext("2d");
+//     context.drawImage(video, 0, 0, canvas.width, canvas.height);
+//     const imageData = canvas.toDataURL("image/jpeg", 0.8).split(",")[1];
+
+//     try {
+//         const response = await fetch("/api/recognize", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({ image: imageData })
+//         });
+
+//         const result = await response.json();
+//         recognizedSigns.push(result.sign);
+//         updateResultDisplay();
+//     } catch (error) {
+//         console.error("Error sending frame to the server:", error);
+//     }
+// }
+
+// function updateResultDisplay() {
+//     /* 
+//         * This function updates the current results that we have ... we'll modify to have a sentence and
+//         * not bullet points ... 
+//         */
+//     resultDiv.innerHTML = "<ul>";
+//     recognizedSigns.forEach(sign => {
+//         resultDiv.innerHTML += `<li>${sign}</li>`;
+//     });
+//     resultDiv.innerHTML += "</ul>";
+// }
 
 let recordedChunks = [];
 let mediaRecorder;
